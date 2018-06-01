331f2dba09c9179c0db59a34d5b827c53085a869
NTP OK <39792896+ntpok@users.noreply.github.com>
2018-06-01 09:37:45 +0800

    Add files via upload

--- ntpok-update.sh
+++ ntpok-update.sh
@@ -3,7 +3,7 @@ cd /home/ntpok/ntpok
 python2 ntpok-check.py > ntpok-check-output.txt
 python2 ntpok-page.py
 cd /home/ntpok
-git clone https://github.com/ntpok/ntpok.github.io.git
+git clone git@github.com:ntpok/ntpok.github.io.git
 cd /home/ntpok/ntpok.github.io
 rm -vf index.html
 cp /home/ntpok/ntpok/ntpok-page-output.html /home/ntpok/ntpok.github.io/index.html
